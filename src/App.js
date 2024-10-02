586f53c06d0c9e399d56bf8bb6613432bcd41cd1
kanmaniashokqa <kanmani818@gmail.com>
2024-10-02 21:23:49 +0200

    browser router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './contexts/AuthContext';
 import Navigation from './components/Navigation';
 import BlogList from './components/BlogList';
