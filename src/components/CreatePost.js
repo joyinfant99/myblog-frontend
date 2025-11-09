98d40edc1fc9f9eedc6d031e879c78e2a79e0dab
joyinfant99 <joyinfant99@gmail.com>
2025-11-09 21:04:15 +0100

    Remove unused useNavigate import

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import ReactQuill from 'react-quill';
 import VoiceAudioGenerator from './VoiceAudioGenerator';
